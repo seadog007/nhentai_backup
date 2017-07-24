39d6a647dca53f0f0be1aa4e69ff2eb94141784d
Jasper Yu <007seadog@gmail.com>
2017-07-24 11:53:56 +0800

    Added IP & port

--- dump_data.sh
+++ dump_data.sh
@@ -2,5 +2,5 @@
 
 while read line
 do
-mongo nhentai --eval "db.nhentai.insert(`curl -v --proxy proxy.hinet.net:80 https://nhentai.net/g/$line/ | grep -oP 'var gallery = new N.gallery\(\K.*(?=\);)'`)"
+mongo 127.0.0.1:18888 nhentai --eval "db.nhentai.insert(`curl -v --proxy proxy.hinet.net:80 https://nhentai.net/g/$line/ | grep -oP 'var gallery = new N.gallery\(\K.*(?=\);)'`)"
 done < list
