bc17ec297d99740790a4cd9cf6b15336bafc9afc
Jasper Yu <007seadog@gmail.com>
2017-07-24 11:09:29 +0800

    Added trap and dynamic range

--- get_id_list.sh
+++ get_id_list.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for i in `seq 7889 7891`
+function finish {
+	./unique.sh list
+}
+trap finish EXIT
+
+for i in `seq $1`
 do
 	echo -e "Page:\t$i"
 	curl -s --proxy proxy.hinet.net:80 "https://nhentai.net/?page=$i" | grep -oP 'href="/g/\K[0-9]+' >> list
 done
-./unique.sh list
