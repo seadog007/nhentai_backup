47478d2180bea5fb735898016b8d3189a3581a82
Jasper Yu <007seadog@gmail.com>
2017-07-25 23:30:05 +0800

    Fixed Database name for dump_data.sh

--- dump_data.sh
+++ dump_data.sh
@@ -5,7 +5,7 @@ function download {
 	if [ -z "`grep $line json_fetched`" ]
 	then
 		echo "Fetching $line"
-		mongo 127.0.0.1:18888 nhentai --eval "db.nhentai.insert(`curl --proxy proxy.hinet.net:80 https://nhentai.net/g/$line/ | grep -oP 'var gallery = new N.gallery\(\K.*(?=\);)'`)"
+		mongo 127.0.0.1:18888/nhentai --eval "db.nhentai.insert(`curl --proxy proxy.hinet.net:80 https://nhentai.net/g/$line/ | grep -oP 'var gallery = new N.gallery\(\K.*(?=\);)'`)"
 		echo "$line" >> json_fetched
 	else
 		echo "$line Already Fetched"
