fff7c11a6b11dde629e7042a4702b7db1b36e2e1
Jasper Yu <007seadog@gmail.com>
2017-07-26 02:22:05 +0800

    Improved CLI UI

--- dump_data.sh
+++ dump_data.sh
@@ -3,16 +3,17 @@
 if [ -n "$1" ]
 then
     line=$1
+	clear
 	if [ -z "`grep $line json_fetched`" ]
 	then
 		echo "Fetching $line"
-		mongo 127.0.0.1:18888/nhentai --eval "db.nhentai.insert(`curl --proxy proxy.hinet.net:80 https://nhentai.net/g/$line/ | grep -oP 'var gallery = new N.gallery\(\K.*(?=\);)'`)"
+		mongo 127.0.0.1:18888/nhentai --eval "db.nhentai.insert(`curl -s --proxy proxy.hinet.net:80 https://nhentai.net/g/$line/ | grep -oP 'var gallery = new N.gallery\(\K.*(?=\);)'`)"
 		echo "$line" >> json_fetched
 	else
 		echo "$line Already Fetched"
 	fi
 else
 	#reqiored GNU parallel that has --bar (20161222)
-	cat list | parallel --bar $0
+	cat list | parallel --bar -j 50 $0
 fi
 
