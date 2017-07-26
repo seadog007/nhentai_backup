51caa13472a77a489670d099214e42d339ffcdc1
Jasper Yu <007seadog@gmail.com>
2017-07-26 15:33:16 +0800

    Exporting mediaid from db in dump_pagenum.sh

--- mongodb/dump_pagenum.sh
+++ mongodb/dump_pagenum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mongoexport --host 127.0.0.1 --port 18888 -dnhentai -cnhentai --fields id,num_pages --type=csv --out temp
+mongoexport --host 127.0.0.1 --port 18888 -dnhentai -cnhentai --fields id,num_pages,media_id --type=csv --out temp
 sed -i 1d temp
 sort -t, -k1n,1 temp > pagenum.csv
 rm temp
