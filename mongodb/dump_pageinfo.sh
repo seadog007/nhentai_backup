e58c6d8ad8e945024021fa7f8376ed4edc6d8751
Jasper Yu <007seadog@gmail.com>
2017-07-26 18:08:06 +0800

    Exporting cover & page type in dump_pageinfo.sh

--- mongodb/dump_pageinfo.sh
+++ mongodb/dump_pageinfo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mongoexport --host 127.0.0.1 --port 18888 -dnhentai -cnhentai --fields id,num_pages,media_id --type=csv --out temp
-sed -i 1d temp
+mongoexport --host 127.0.0.1 --port 18888 -dnhentai -cnhentai --fields id,num_pages,media_id,images \
+	| jq -r '[(.id|tostring),(.num_pages|tostring),.media_id,([.images.cover.t]|join("")),([.images.pages[].t]|join(""))]|join(",")' > temp
 sort -t, -k1n,1 temp > pageinfo.csv
 rm temp
