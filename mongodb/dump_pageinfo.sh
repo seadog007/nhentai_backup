5439dba5971dd5045dea067858e8d7adaf222197
Jasper Yu <007seadog@gmail.com>
2017-07-26 15:36:42 +0800

    Remaned dump_pagenum.sh to dump_pageinfo.sh

--- mongodb/dump_pageinfo.sh
+++ mongodb/dump_pageinfo.sh
@@ -2,5 +2,5 @@
 
 mongoexport --host 127.0.0.1 --port 18888 -dnhentai -cnhentai --fields id,num_pages,media_id --type=csv --out temp
 sed -i 1d temp
-sort -t, -k1n,1 temp > pagenum.csv
+sort -t, -k1n,1 temp > pageinfo.csv
 rm temp
